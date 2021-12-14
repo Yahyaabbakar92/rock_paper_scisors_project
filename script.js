efdd700b546a9222516bedcd9217f8a5d71e9a90
Yahya Abbakar <yahyaabbakar@gmail.com>
2021-12-14 10:52:30 +0200

    Wrote the script for the console game

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+// Variables to store the players' score and CPUs' score
+let computerScore = 0;
+let playerScore = 0;
+// A variable to store the number of rounds played
+let numberOfRounds = 0;
+
 // A function that plays a round of rock paper scissors
 function playRound() {
-  // A function that stores the players input
+  // A variable that stores the players input
   let playerInput = prompt("Please select rock, paper or scissors");
   let playerSelection = playerInput.toLowerCase();
 
@@ -13,28 +19,55 @@ function playRound() {
   }
   let computerSelection = computerPlay();
 
-  // The logic of the game
+  // The logic of the game including storing the scores for both player and CPU
   if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")
   ) {
-    return "You win this round";
+    ++playerScore;
+    computerScore += 0;
+    ++numberOfRounds;
+    console.log(
+      `You win this round. Your score is ${playerScore} and the CPU's score is ${computerScore}.`
+    );
   } else if (
     (playerSelection === "scissors" && computerSelection === "rock") ||
     (playerSelection === "paper" && computerSelection === "scissors") ||
     (playerSelection === "rock" && computerSelection === "paper")
   ) {
-    return "You lose this round";
+    playerScore += 0;
+    ++computerScore;
+    ++numberOfRounds;
+    console.log(
+      `You lose this round. Your score is ${playerScore} and the CPU's score is ${computerScore}.`
+    );
   } else if (
     (playerSelection === "rock" && computerSelection === "rock") ||
     (playerSelection === "paper" && computerSelection === "paper") ||
     (playerSelection === "scissors" && computerSelection === "scissors")
   ) {
-    return "Its a tie";
+    playerScore += 0;
+    computerScore += 0;
+    ++numberOfRounds;
+    console.log(
+      `Its a tie. Your score is ${playerScore} and the CPU's score is ${computerScore}.`
+    );
   } else {
-    return "Please select rock, paper or scissors";
+    return prompt("Please select rock, paper or scissors");
   }
 }
 
-console.log(playRound());
+// A function to play 5 rounds of the game
+function game() {
+  while (numberOfRounds < 5) {
+    playRound();
+    if (numberOfRounds === 5) {
+      console.log(
+        `Game Over. Your score is ${playerScore} and the CPU's score is ${computerScore}.`
+      );
+      break;
+    }
+  }
+}
+game();
