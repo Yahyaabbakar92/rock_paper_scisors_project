6772d79b4e233183c9ecfa382492dc38e239d7da
Yahya Abbakar <yahyaabbakar@gmail.com>
2022-03-03 18:45:35 +0200

    redid the UI using JS

--- script.js
+++ script.js
@@ -1,71 +1,63 @@
-// let playerOptions = prompt("Please select rock, paper or scissors");
-// let playerOption = playerOptions.toLowerCase();
-
-const computerOptions = ["rock", "paper", "scissors"];
-// The function that randomly selects the computer option from the computerOptions array
-function computerPlay() {
-  return computerOptions[Math.floor(Math.random() * computerOptions.length)];
-}
-let computerOption = computerPlay();
+const buttons = document.querySelectorAll(".btns");
+const results = document.getElementById("results");
 let userScore = 0;
 let computerScore = 0;
-let numberOfRounds = 0;
 
-// null toLowerCase isn't an object and returns a type error
+// The function to start the game
+buttons.forEach((button) =>
+  button.addEventListener("click", function () {
+    playRound(button.value, computerPlay());
 
-// The function for playing a round
+    const result = document.createElement("div");
+    result.classList.add("result");
+    result.textContent = `User Score:${userScore}
+    Computer Score:${computerScore}`;
+    results.appendChild(result);
+  })
+);
+
+// The function that randomly selects the computer option from the computerOptions array
+function computerPlay() {
+  const computerOptions = ["rock", "paper", "scissors"];
+  return computerOptions[Math.floor(Math.random() * computerOptions.length)];
+}
+
+// The logic to play the game of rps
 function playRound(playerSelection, computerSelection) {
-  let playerOptions = prompt("Please select rock, paper or scissors");
-  let playerOption = playerOptions.toLowerCase();
   if (
-    (playerOption === "rock" && computerOption === "rock") ||
-    (playerOption === "paper" && computerOption === "paper") ||
-    (playerOption === "scissors" && computerOption === "scissors")
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
   ) {
-    userScore += 0;
-    computerScore += 0;
-    numberOfRounds++;
-    console.log(`Draw! ${playerOption} equals ${computerOption}`);
-  } else if (
-    (playerOption === "rock" && computerOption === "scissors") ||
-    (playerOption === "paper" && computerOption === "rock") ||
-    (playerOption === "scissors" && computerOption === "paper")
-  ) {
-    userScore++;
-    computerScore += 0;
-    numberOfRounds++;
-    console.log(`You win this round! ${playerOption} beats ${computerOption}`);
-  } else if (
-    (playerOption === "rock" && computerOption === "paper") ||
-    (playerOption === "paper" && computerOption === "scissors") ||
-    (playerOption === "scissors" && computerOption === "rock")
-  ) {
-    userScore += 0;
-    computerScore++;
-    numberOfRounds++;
-    console.log(`You lose this round! ${computerOption} beats ${playerOption}`);
-  } else {
-    userScore += 0;
-    computerScore += 0;
-    numberOfRounds += 0;
-    console.log("Please input rock, paper or scissors");
-  }
-}
+    userScore += 1;
 
-// playRound(playerOption, computerOption);
-
-function game() {
-  for (let i = 0; i < 5; i++) {
-    playRound();
-    if (numberOfRounds === 5 && userScore > computerScore) {
-      console.log(
-        `Game over! You won! You scored ${userScore} and the computer scored ${computerScore}`
-      );
-    } else if (numberOfRounds === 5 && userScore < computerScore) {
-      console.log(
-        `Game over! You lost! You scored ${userScore} and the computer scored ${computerScore}`
-      );
+    results.textContent = `You won the round! ${playerSelection} beats ${computerSelection}`;
+    if (userScore === 5) {
+      disableButtons();
+      results.textContent =
+        "Game over! You've won. Reload the page to play again";
     }
+  } else if (
+    (playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "rock")
+  ) {
+    computerScore += 1;
+
+    results.textContent = `You lost the round! ${computerSelection} beats ${playerSelection}`;
+    if (computerScore === 5) {
+      disableButtons();
+      results.textContent =
+        "Game over! You've lost. Reload the page to play again";
+    }
+  } else {
+    results.textContent = `It's a tie! ${playerSelection} equals ${computerSelection}`;
   }
 }
-game();
+
+// The function for restarting the game
+function disableButtons() {
+  buttons.forEach((elem) => {
+    elem.disabled = true;
+  });
+}
